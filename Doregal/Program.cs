a57116d9f2891447301de518c1e82a60e821243b
Jiahua Zhang <jiahuaz@yahoo.com>
2020-01-13 15:53:01 -0600

    Add project files.

---
+++ Doregal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Doregal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
